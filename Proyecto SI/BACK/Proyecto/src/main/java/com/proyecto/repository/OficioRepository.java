712a3508386fbe05ea50877f13735946f448954c
RaulSolis <raulsolis1606@gmail.com>
2025-11-26 07:06:32 -0600

    fix 3

--- Proyecto SI/BACK/Proyecto/src/main/java/com/proyecto/repository/OficioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.proyecto.model.Oficio;
-
-public interface OficioRepository extends CrudRepository<Oficio, String> {
-}
